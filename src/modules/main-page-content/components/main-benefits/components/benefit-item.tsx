71ea16fe22a565df41e3ae9143d973016d599e6c
IgorBurunduk <burunduk-one@yandex.ru>
2025-09-04 20:45:21 +0500

    feat: BLOCK3-3 created base for benefit-item

---
+++ src/modules/main-page-content/components/main-benefits/components/benefit-item.tsx
@@ -0,0 +1,21 @@
+import type { BenefitItemType } from '@/types/benefits';
+
+import styles from './benefit-item.module.scss';
+
+interface BenefitItemProps {
+  benefitProps: BenefitItemType;
+}
+
+export const BenefitItem = ({
+  benefitProps: { subtitle, description, tooltip },
+}: BenefitItemProps) => {
+  return (
+    <div className={styles.benefitItem}>
+      <div className={styles.benefitHeader}>
+        <h3 className={styles.benefitTitle}>{subtitle}</h3>
+        {Boolean(tooltip) && (<div>{tooltip}</div>)}
+      </div>
+      <p className={styles.benefitDescription}>{description}</p>
+    </div>
+  );
+};
