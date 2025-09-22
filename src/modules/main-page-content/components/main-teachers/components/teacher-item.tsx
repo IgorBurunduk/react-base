df71a59b31c743d153985d366339744afbdc691e
IgorBurunduk <burunduk-one@yandex.ru>
2025-09-22 20:03:16 +0500

    refactor: BLOCK3-5 change imageSrc in TeacherItem

--- src/modules/main-page-content/components/main-teachers/components/teacher-item.tsx
+++ src/modules/main-page-content/components/main-teachers/components/teacher-item.tsx
@@ -1,13 +1,14 @@
+import * as teachersImages from '@/assets/images';
 import { Link } from '@/components/link';
 import type TeacherListType from '@/types/teacher';
 
 import styles from './teacher-item.module.scss';
 
 export const TeacherItem = ({ id, name, imageSrc, description }: TeacherListType) => {
-
+  const teacherImageSrc = teachersImages.teachersImages[imageSrc];
   return (
     <div className={styles.teacherItem}>
-      <img src={`../src/assets/images/${imageSrc}.png`} alt={name} className={styles.teacherItemPhoto} />
+      <img src={teacherImageSrc} alt={name} className={styles.teacherItemPhoto} />
       <h3 className={styles.teacherItemName}>{name}</h3>
       <p className={styles.teacherItemDescription}>{description}</p>
       <Link
