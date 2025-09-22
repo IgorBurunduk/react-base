1add08657e492c06ada606ce07d03ef79f000b7d
IgorBurunduk <burunduk-one@yandex.ru>
2025-09-22 19:17:22 +0500

    feat: BLOCK3-5 added basic component TeacherItem

---
+++ src/modules/main-page-content/components/main-teachers/components/teacher-item.tsx
@@ -0,0 +1,19 @@
+import { Link } from '@/components/link';
+import type TeacherListType from '@/types/teacher';
+
+import styles from './teacher-item.module.scss';
+
+export const TeacherItem = ({ id, name, imageSrc, description }: TeacherListType) => {
+
+  return (
+    <div className={styles.teacherItem}>
+      <img src={`../src/assets/images/${imageSrc}.png`} alt={name} className={styles.teacherItemPhoto} />
+      <h3 className={styles.teacherItemName}>{name}</h3>
+      <p className={styles.teacherItemDescription}>{description}</p>
+      <Link
+        href={`/teacher/${id}`}
+        additionalClassname={styles.teacherItemLink}
+      >{`Подробнее`}</Link>
+    </div>
+  );
+};
--- src/modules/main-page-content/components/main-teachers/main-teachers.tsx
+++ src/modules/main-page-content/components/main-teachers/main-teachers.tsx
@@ -3,18 +3,23 @@ import { useContext } from 'react';
 import { Container } from '@/components/container';
 import { MainPageContext } from '@/store/main-page';
 
+import {TeacherItem} from './components/teacher-item';
+
 import styles from './main-teachers.module.scss';
 
 export const MainTeachers = () => {
   const teachers = useContext(MainPageContext);
-
-  console.log(teachers);
 
   return (
     <section className={styles.teachers}>
       <Container>
         <div className={styles.wrapper}>
           <h2 className={styles.title}>Профессиональные тренеры</h2>
+          <div className={styles.teachersList}>
+            {teachers.teachersList.map((teacherItem) => (
+              <TeacherItem key={teacherItem.id} {...teacherItem} />
+            ))}
+          </div>
         </div>
       </Container>
     </section>
